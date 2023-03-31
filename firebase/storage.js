826185fef5560b6b26d64bbfb0cb5078a94e1343
Giovanna Radica <g.radica@be-tse.it>
2023-03-31 18:20:00 +0200

    feature: implement cloud storage

--- firebase/storage.js
+++ firebase/storage.js
@@ -15,6 +15,22 @@
  * limitations under the License.
  */
 
-import { format } from 'date-fns';
-import { deleteObject, getDownloadURL as getStorageDownloadURL, ref, uploadBytes } from 'firebase/storage';
-import { storage } from './firebase';
+import { format } from "date-fns";
+import {
+  deleteObject,
+  getDownloadURL as getStorageDownloadURL,
+  ref,
+  uploadBytes,
+} from "firebase/storage";
+import { storage } from "./firebase";
+
+const BUCKET_URL = "gs://expense-tracker-9ae69.appspot.com";
+
+export const uploadImage = async (image, uid) => {
+  const formattedDate = format(new Date(), "dd-MM-yyyy'T'HH:mm:ss'Z'");
+  //Il bucket serve per inserire i dati nel DB
+  const bucket = `${BUCKET_URL}/${uid}/${formattedDate}.jpg`;
+  const storageRef = ref(storage, bucket);
+  await uploadBytes(storageRef, image);
+  return bucket;
+};
